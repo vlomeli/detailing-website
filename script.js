77032c6683e75148371e0f0da8275f9d2f19db1d
vlomeli <victorlomeli2023@gmail.com>
2024-07-12 15:09:26 -0700

    made changes to structure

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Initialize EmailJS
+(function () {
+  emailjs.init("MAlFkml9eGdPHhKb4");
+})();
 
+// Handle form submission
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("service_w2spjch", "template_xdtgrec", this).then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("Email has been sent!");
+          document.getElementById("contact-form").reset();
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+});

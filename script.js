35dff81949fb4362ea24d72727b621d9569c6cc3
vlomeli <victorlomeli2023@gmail.com>
2024-07-18 14:43:13 -0700

    Added reveal to the content

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
       );
     });
 });
+
+// content reveal
+window.addEventListener('scroll', reveal);
+function reveal () {
+  var reveals = document.querySelectorAll('.reveal')
+
+  for (var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('activate');
+    } else {
+      reveals[i].classList.remove("activate");
+    };
+  };
+};
